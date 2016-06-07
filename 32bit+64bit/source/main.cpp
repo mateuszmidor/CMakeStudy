f8c1319b40926a53c89917743af8e74e13e3bcd9
Mateusz Midor <mateusz>
2016-06-07 07:28:16 +0200

    Added 32bit+64bit one pass build example

---
+++ 32bit+64bit/source/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 30, 2016
+ *      Author: mateusz
+ */
+
+#include "threadingexample.h"
+
+int main() {
+   return runDemo();
+}
---
+++ 32bit+64bit/threadingexample/source/threadingexample.h
@@ -0,0 +1,13 @@
+/*
+ * threadingexample.h
+ *
+ *  Created on: Jun 6, 2016
+ *      Author: mateusz
+ */
+
+#ifndef THREADINGEXAMPLE_SOURCE_THREADINGEXAMPLE_H_
+#define THREADINGEXAMPLE_SOURCE_THREADINGEXAMPLE_H_
+
+int runDemo();
+
+#endif /* THREADINGEXAMPLE_SOURCE_THREADINGEXAMPLE_H_ */
